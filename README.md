c8dea5b61b157a8c56952c23ad09a762d32be9cb
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-06-20 10:48:33 -0700

    add mouse events

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # web-ad
 https://hellotakako.github.io/web-ad/
+
+
+#Refs
+https://shopify.github.io/draggable/examples/ <br>
+https://javascript.info/mouse-drag-and-drop
--- js/script.js
+++ js/script.js
@@ -4,12 +4,24 @@ const SVG3=document.getElementById("svg3");
 const FLOWER1=document.getElementById("img1");
 const FLOWER2=document.getElementById("img2");
 const FLOWER3=document.getElementById("img3");
+const circle = document.getElementsByClassName("circle");
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
 
-TweenMax.from(SVG1, 0.5, {
-    opacity:0,
-    scale:0,
-    ease:Bounce.easeOut
-});
+
+// TweenMax.from(c2, 0.5, {
+//     opacity:0,
+//     scale:0,
+//     ease:Bounce.easeOut
+// });
+
+
+
+// TweenMax.from(SVG1, 0.5, {
+//     opacity:0,
+//     scale:0,
+//     ease:Bounce.easeOut
+// });
 
 
 var wrap = document.getElementsByClassName("wrapper");
@@ -20,6 +32,17 @@ function m(e){
         FLOWER2.style.top=e.pageY/50 + "px";
         FLOWER3.style.right=e.pageX/100 + "px";
         FLOWER3.style.top=e.pageY/100 + 40 + "px";
+}
+var inner = document.getElementById('wrapper-inner');
+c2.addEventListener('mousedown', move);
+function move(e){
+    console.log(e);
+        c2.style.left = e.pageX;
+        c2.style.top = e.pageY;
+}
+function drag(e){
+    c2.style.left = e.pageX;
+    c2.style.top = e.pageY;
 }
 
 document.addEventListener("mousemove", m);
