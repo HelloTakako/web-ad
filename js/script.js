1aec0d1ee3db6028fdfe0214e3945ada967d712f
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-06-20 21:36:36 -0700

    add background image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-const SVG1=document.getElementById("svg1");
-const SVG2=document.getElementById("svg2");
-const SVG3=document.getElementById("svg3");
 const FLOWER1=document.getElementById("img1");
 const FLOWER2=document.getElementById("img2");
 const FLOWER3=document.getElementById("img3");
+const FLOWER4=document.getElementById("img4");
 const circle = document.getElementsByClassName("circle");
 const c1 = document.getElementById('c1');
 const c2 = document.getElementById('c2');
@@ -26,26 +24,33 @@ const c2 = document.getElementById('c2');
 
 var wrap = document.getElementsByClassName("wrapper");
 function m(e){    
-        FLOWER1.style.left=e.pageX/10 + "px";
-        FLOWER1.style.top=e.pageY/10 + "px";
+        FLOWER1.style.left=e.pageX/10 + 200+ "px";
+        FLOWER1.style.top=e.pageY/10  + "px";
         FLOWER2.style.left=e.pageX/50 + "px";
         FLOWER2.style.top=e.pageY/50 + "px";
         FLOWER3.style.right=e.pageX/100 + "px";
         FLOWER3.style.top=e.pageY/100 + 40 + "px";
+        FLOWER4.style.right=e.pageX/30 + 300 + "px";
+        FLOWER4.style.top=e.pageY/30 + 100 + "px";
 }
 
 var inner = document.getElementById('wrapper-inner');
-// c2.addEventListener('mousedown', move);
-// function move(e){
-//     console.log(e);
-//         c2.style.left = e.pageX;
-//         c2.style.top = e.pageY;
-// }
-// function drag(e){
-//     c2.style.left = e.pageX;
-//     c2.style.top = e.pageY;
-// }
 
 document.addEventListener("mousemove", m);
 
-// droppable
+//
+// drag and drop (https://www.w3schools.com/html/html5_draganddrop.asp)
+//
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
